a8a6cfe076af5982bdbb2584aa175765a5eab20a
Wender Patrick <outrowender@gmail.com>
2020-01-12 23:24:05 -0300

    testes adicionados para o projeto de login

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -6,6 +6,7 @@ import {SharedModule} from 'src/app/shared/shared.module'
 import {HomeModule} from 'src/app/home/home.module'
 import {RouterTestingModule} from '@angular/router/testing'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {password} from '@rxweb/reactive-form-validators'
 describe('LoginComponent', () => {
   let component: LoginComponent
   let fixture: ComponentFixture<LoginComponent>
@@ -26,5 +27,42 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('shoud render main form', () => {
+    const fixture = TestBed.createComponent(LoginComponent)
+    const comp = fixture.componentInstance
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.nativeElement
+
+    const loginInput = compiled.querySelector('input[id="email"]')
+    const passwordInput = compiled.querySelector('input[id="password"]')
+    const submitButton = compiled.querySelector('button[type="submit"]')
+
+    expect(loginInput).toBeTruthy()
+    expect(passwordInput).toBeTruthy()
+    expect(submitButton).toBeTruthy()
+  })
+
+  it('shoud have a "create account" button', () => {
+    const fixture = TestBed.createComponent(LoginComponent)
+    const comp = fixture.componentInstance
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.nativeElement
+
+    const accountButton = compiled.querySelector('a[href="/register"]')
+
+    expect(accountButton).toBeTruthy()
+  })
+
+  it('shoud have a main icon', () => {
+    const fixture = TestBed.createComponent(LoginComponent)
+    const comp = fixture.componentInstance
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.nativeElement
+
+    const icon = compiled.querySelector('img[alt="Angular Logo"]')
+
+    expect(icon).toBeTruthy()
   })
 })
